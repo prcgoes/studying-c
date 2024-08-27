bd9f828d70d3132b9d0fe58a07e7918547970432
Priscila <priticouto0209@gmail.com>
2024-08-27 19:38:00 -0300

    Aula 01 -  Aprendendo sobre entrada, processos e saidas

---
+++ exemplo-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    float n1,n2,m;
+    printf("Ler o valor da primeira nota: ");
+    scanf("%f" ,&n1);
+    printf("Ler o valor da segunda nota: ");
+    scanf("%f", &n2);
+    m=(n1+n2)/2;
+    printf("Media = %.2f\n" ,m);
+
+
+    return (0);
+}
